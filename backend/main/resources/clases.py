8841e5a720bac0746d9c7a6a797a11b9a956d0e7
FrancoBertoldiMariglio <f.bertoldi@alumno.um.edu.ar>
2023-05-08 21:06:50 -0300

    arreglo de queries

--- backend/main/resources/clases.py
+++ backend/main/resources/clases.py
@@ -22,9 +22,9 @@ class Clases(Resource):
         if request.args.get('clase'):
             clases = clases.join(ClasesModel.planificacion).filter_by(clases.tipo.like(request.args.get('clase')))
             
-        # d
-        if request.args.get('sort_by_planificaciones'):
-            clases = clases.join(ClasesModel.planificacion).group_by(ClasesModel.tipo).order_by(desc(func.count(ClasesModel.id_alumno)))
+        # if request.args.get('sort_by_planificaciones'):
+        #     clases = clases.join(ClasesModel.planificacion).group_by(ClasesModel.tipo).order_by(desc(func.count(ClasesModel.id_alumno)))
+        
         try:
             clases = clases.paginate(page=page, per_page=per_page, error_out=True, )
         except:
--- backend/main/resources/profesor.py
+++ backend/main/resources/profesor.py
@@ -36,6 +36,11 @@ class Profesores(Resource):
             segundo_join = primer_join.join(ClasesModel, ClasesModel.id_clase == profesores_clases.id_clase)
             profesores = segundo_join.order_by(ProfesorModel.id_profesor)
 
+            """SELECT p.id_profesor, c.id_clase 
+            FROM profesor p JOIN profesores_clases pc ON p.id_profesor = pc.id_profesor 
+            JOIN clase c ON c.id_clase = pc.id_clase 
+            ORDER BY p.id_profesor """
+
         # devuelve los profesores con sus alumnos (chequear)
         if request.args.get('alumnos'):
             query = db.session.query(ProfesorModel.id_profesor, AlumnoModel.id_alumno)\
@@ -44,6 +49,16 @@ class Profesores(Resource):
                 .join(PlanificacionModel.alumnos)\
                 .join(AlumnoModel)\
                 .all()
+            
+            """SELECT u.nombre, u.apellido, COUNT(a.id_alumno) AS cantidad_alumnos
+            FROM usuario u JOIN profesor p ON u.id_usuario = p.id_usuario 
+            JOIN profesores_clases pc ON p.id_profesor = pc.id_profesor
+            JOIN clase c ON c.id_clase = pc.id_clase
+            JOIN planificacion pl ON pl.id_clase  = c.id_clase 
+            JOIN alumnos_planificaciones ap ON pl.id_planificacion = ap.id_planificacion 
+            JOIN alumno a ON a.id_alumno = ap.id_alumno 
+            GROUP BY u.nombre 
+            ORDER BY u.nombre DESC"""
 
             result = [{'id_profesor': row[0], 'id_alumno': row[1]} for row in query]
 
