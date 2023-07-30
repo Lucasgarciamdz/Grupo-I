ad559dbaaa93d43e1a5242b987ec931512b599ca
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2023-07-30 17:37:19 -0300

    funciona el docker compose

--- backend/main/auth/rutas.py
+++ backend/main/auth/rutas.py
@@ -40,63 +40,63 @@ def login():
 @auth.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    # usuario = None
-    # if "usuario" in data:
-    #     usuario_data = data["usuario"]
-    #     exists = db.session.query(UsuarioModel).filter(
-    #         (UsuarioModel.email == usuario_data['email']) |
-    #         (UsuarioModel.dni == usuario_data['dni']) |
-    #         (UsuarioModel.telefono == usuario_data['telefono'])
-    #     ).scalar() is not None
-    #     if exists:
-    #         return 'Duplicated email, dni, or telefono'
-    #     else:
-    #         usuario = UsuarioModel.from_json(usuario_data)
-    profesor = ProfesorModel.from_json(data["profesor"])
-    if "alumno" in data:
-        alumno = AlumnoModel.from_json(data["alumno"])
-    response_profesor = None
-    response_alumno = None
-    usuario = UsuarioModel.from_json(data["usuario"])
-    try:
-        db.session.add(usuario)
-    except Exception:
-        db.session.rollback()
-        return "No se registro ningun usuario"
+    usuario = None
+    if "usuario" in data:
+        usuario_data = data["usuario"]
+        exists = db.session.query(UsuarioModel).filter(
+            (UsuarioModel.email == usuario_data['email']) |
+            (UsuarioModel.dni == usuario_data['dni']) |
+            (UsuarioModel.telefono == usuario_data['telefono'])
+        ).scalar() is not None
+        if exists:
+            return 'Duplicated email, dni, or telefono'
+        else:
+            usuario = UsuarioModel.from_json(usuario_data)
+    # profesor = ProfesorModel.from_json(data["profesor"])
+    # if "alumno" in data:
+    #     alumno = AlumnoModel.from_json(data["alumno"])
+    # response_profesor = None
+    # response_alumno = None
+    # usuario = UsuarioModel.from_json(data["usuario"])
+    # try:
+    #     db.session.add(usuario)
+    # except Exception:
+    #     db.session.rollback()
+    #     return "No se registro ningun usuario"
 
-    if profesor:
-        profesor_dict = profesor.to_json()
-        last_usuario = db.session.query(UsuarioModel).order_by(UsuarioModel.id_usuario.desc()).first()
-        profesor_dict['id_usuario'] = last_usuario.id_usuario
-        try:
-            response_profesor = ProfesoresResource().post(data={"profesor": profesor_dict, "clase": data["clase"]})
-        except Exception:
-            db.session.rollback()
-            return "No hay clase" 
+    # if profesor:
+    #     profesor_dict = profesor.to_json()
+    #     last_usuario = db.session.query(UsuarioModel).order_by(UsuarioModel.id_usuario.desc()).first()
+    #     profesor_dict['id_usuario'] = last_usuario.id_usuario
+    #     try:
+    #         response_profesor = ProfesoresResource().post(data={"profesor": profesor_dict, "clase": data["clase"]})
+    #     except Exception:
+    #         db.session.rollback()
+    #         return "No hay clase" 
 
-    elif alumno:
-        alumno_dict = alumno.to_json()
-        last_usuario = db.session.query(UsuarioModel).order_by(UsuarioModel.id_usuario.desc()).first()
-        alumno_dict['id_usuario'] = last_usuario.id_usuario
-        try:
-            response_alumno = AlumnosResource.post(data={"alumno": alumno_dict, "planificacion": data["planificacion"]})
-        except Exception:
-            db.session.rollback()
-            return "No hay planificacion"
+    # elif alumno:
+    #     alumno_dict = alumno.to_json()
+    #     last_usuario = db.session.query(UsuarioModel).order_by(UsuarioModel.id_usuario.desc()).first()
+    #     alumno_dict['id_usuario'] = last_usuario.id_usuario
+    #     try:
+    #         response_alumno = AlumnosResource.post(data={"alumno": alumno_dict, "planificacion": data["planificacion"]})
+    #     except Exception:
+    #         db.session.rollback()
+    #         return "No hay planificacion"
 
     db.session.commit()
 
-    response_data = {}
-    if usuario:
-        response_data['usuario'] = usuario.to_json()
-    if response_profesor:
-        response_data['profesor'] = response_profesor
-    if response_alumno:
-        response_data['alumno'] = response_alumno
-    if response_data:
-        return response_data
-    else:
-        return 'No data provided'
+    # response_data = {}
+    # if usuario:
+    #     response_data['usuario'] = usuario.to_json()
+    # if response_profesor:
+    #     response_data['profesor'] = response_profesor
+    # if response_alumno:
+    #     response_data['alumno'] = response_alumno
+    # if response_data:
+    #     return response_data
+    # else:
+    #     return 'No data provided'
     # except Exception:
     #     db.session.rollback()
     #     return "500"
--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM python
+FROM python:3.11.4-alpine
 
 COPY . .
 
