f4efb37e53450fbf9c034014db807a5c80208d04
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2023-10-10 17:02:27 -0300

    WIP: clases-list

--- frontend/gym_app/src/app/components/clases-list/clases-list.component.ts
+++ frontend/gym_app/src/app/components/clases-list/clases-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, NgModule, OnInit, ViewChild} from '@angular/core';
+import {UsuariosService} from 'src/app/services/usuarios.service';
+import {FormsModule, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-clases-list',
@@ -7,4 +9,81 @@ import { Component } from '@angular/core';
 })
 export class ClasesListComponent {
 
+
+  @ViewChild('userForm')
+  userForm!: NgForm;
+
+  editingUserId: number = 0;
+
+  users: any[] | undefined;
+
+  constructor(private userSvc: UsuariosService) {
+  }
+
+  ngOnInit() {
+    this.userSvc.getUsers().subscribe({
+      next: (users: any) => {
+        this.users = users.usuario;
+      },
+      error: (error) => {
+        alert('Error al obtener usuarios');
+      },
+      complete: () => {
+        console.log('Finalizó');
+      }
+    });
+  }
+
+
+  editUser(id: number): void {
+    this.editingUserId = id;
+    alert('Editar usuario con id ' + id)
+  }
+
+  deleteUser(id: number): void {
+    this.userSvc.deleteUser(id).subscribe({
+      next: (user: any) => {
+        alert('Usuario eliminado');
+      },
+      error: (error) => {
+        alert('Error al eliminar usuario');
+      },
+      complete: () => {
+        console.log('Finalizó');
+      }
+    });
+  }
+
+
+  saveUser(form: NgForm): void {
+    alert('Guardar usuario')
+    const {nombre, rol, edad} = form.value;
+
+    const user = {nombre, rol, edad};
+
+    const userJson = JSON.stringify(user);
+    this.userSvc.putUser(this.editingUserId, userJson).subscribe({
+      next: (user: any) => {
+        alert('Usuario actualizado');
+      },
+      error: (error) => {
+        alert('Error al actualizar usuario');
+      },
+      complete: () => {
+        console.log('Finalizó');
+        this.editingUserId = 0
+
+      }
+    });
+  }
+
+
+  cancelEdit(): void {
+    alert('Cancelar edición');
+  }
+  @NgModule({
+    declarations: [ClasesListComponent],
+    imports: [CommonModule, FormsModule],
+    exports: [ClasesListComponent]
+  })
 }
