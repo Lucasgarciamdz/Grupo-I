0df22cf31dfda8d9e8339e0caddb1dafe923bbc0
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2023-10-26 01:16:15 -0300

    WIP: prof-buttons

--- frontend/gym_app/src/app/components/prof-buttons/prof-buttons.component.ts
+++ frontend/gym_app/src/app/components/prof-buttons/prof-buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseService } from 'src/app/services/base.service';
 
 @Component({
   selector: 'app-prof-buttons',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./prof-buttons.component.css']
 })
 export class ProfButtonsComponent {
-  students: string[] = ['Student 1', 'Student 2', 'Student 3', 'Student 4', 'Student 5', 'Student 6'];
+  // students: string[] = ['Student 1', 'Student 2', 'Student 3', 'Student 4', 'Student 5', 'Student 6'];
   classes: string[] = ['Class A', 'Class B', 'Class C', 'Class D', 'Class E', 'Class F'];
+  students: any[] = [];
+
+  constructor(private backSvc: BaseService) { }
+
+  ngOnInit() {
+    this.backSvc.get("/alumnos").subscribe({
+      next: (students: any) => {
+        console.log('Respuesta alumnos: ', students);
+        this.students = students.alumnos;
+      },
+      error: (error) => {
+        alert('Error al obtener estudiantes');
+      },
+      complete: () => {
+        console.log('Finalizó');
+      }
+    });
+  }
+
+
 }
